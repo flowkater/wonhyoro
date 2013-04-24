5405e6809519da933dd0e56681fd74e104dcd2ca
ChoJaewoo <chjw102@gmail.com>
2013-04-24 09:51:15 +0900

    server init

--- app/worker/pushteasers_worker.rb
+++ app/worker/pushteasers_worker.rb
@@ -1,6 +1,6 @@
 class PushteasersWorker
 	include Sidekiq::Worker
-	sidekiq_options retry: false
+	sidekiq_options retry: false, backtrace: true
 
 	def perform(teaser_id)
 		teaser = Teaser.find(teaser_id)
